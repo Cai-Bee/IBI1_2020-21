fc0c81936dcc2a60b2296443d43900e4a94c0fb2
Cai-Bee <shuo.20@intl.zju.edu.cn>
2021-04-20 01:14:58 +0800

    correct model

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,17 +1,9 @@
-r=1.2 #The rate of reproduction of a virus
+r=1.1 #The rate of reproduction of a virus
 I=84 #number of initial infected individuals
 
-# j repersent the round number
-# T is the total number of infected individuals
-# b is newly infected individuals in round j
-# a is the newly infected individuals in round j-1
+T = I
+for i in range(1,6):
+    T += T*r
 
-#Initial situation
-a=I
-T=a
-
-for j in range (1,6):
-    b=a*r #the number of newly infected individuals equals to the number of newly infected individuals in last round
-    T=T+b #add newly infected people into total number of infected individuals
-    a=b #let people infected in this round become the "newly infected individuals in round j-1" in next round
-print(T)
+T = int(T)
+print("The r rate is "+str(r)+". The total number of individuals infected after 5 generations is "+str(T))
