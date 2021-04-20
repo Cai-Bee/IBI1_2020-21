13e7d8eeb37a6a067bd2b306e7dd495b242dd0fb
Cai-Bee <shuo.20@intl.zju.edu.cn>
2021-04-20 15:43:18 +0800

    better fit the assignment requirement

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -1,20 +1,32 @@
-#The code for importing the .csv file works
+#The code for importing the .csv file
 import os
+#sys is used for getting the directory of this file
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-os.chdir("/Users/caishuo/Desktop/IBI1_2020-21/pratical7")
+
+#The following sentence works when the whole cript work.
+os.chdir(sys.path[0])
+#The following sentence works when typing commands directly to my terminal.
+os.chdir("/Users/caishuo/github/IBI1_2020-21/Practical7")
 covid_data = pd.read_csv("full_data.csv")
 
 #There is correct code for showing all columns, and every second row between (andincluding) 0 and 10
-covid_data.iloc[0:12:2, :]
+table1 = covid_data.iloc[0:12:2, :]
+print("\n>all columns, and every second row between (andincluding) 0 and 10")
+print(table1)
 
 #You have successfully used a Boolean to show “total cases” for all rows corresponding to Afghanistan.
-covid_data.loc[covid_data.loc[:,"location"] == "Afghanistan", 'total_cases']
+table2 = covid_data.loc[covid_data.loc[:,"location"] == "Afghanistan", 'total_cases']
+print("\n>total cases” for all rows corresponding to Afghanistan")
+print(table2)
 
 #You have correctly computed the mean and median of new cases for the entire world.
-np.mean(covid_data.loc[covid_data.loc[:,"location"] == "World", 'new_cases'])
-np.median(covid_data.loc[covid_data.loc[:,"location"] == "World", 'new_cases'])
+world_new_cases = covid_data.loc[covid_data.loc[:,"location"] == "World", 'new_cases']
+mean = str(np.mean(world_new_cases))
+median = str(np.median(world_new_cases))
+print("\n>the mean and median of new cases for the entire world are respectively "+mean+" and "+median)
 
 #You have successfully created a boxplot of new cases worldwide.
 boxprops = dict(color  = 'lightblue',facecolor = 'paleturquoise', linewidth=2.0)
@@ -22,32 +34,37 @@ medianprops = dict(linestyle='-.', linewidth=2.0, color='teal')
 capprops = dict(color = 'teal', linewidth=2.0)
 whiskerprops = dict(color = 'lightblue', linewidth=2.0)
 labels = ['new cases worldwide']
-#plot
-fig, ax = plt.subplots()
-ax.boxplot(covid_data.loc[covid_data.loc[:,"location"] == "World", "new_cases"],
+    #plot
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(1,1,1)
+ax1.boxplot(covid_data.loc[covid_data.loc[:,"location"] == "World", "new_cases"],
     patch_artist=True,
     boxprops = boxprops,
     medianprops = medianprops,
     capprops = capprops,
     whiskerprops = whiskerprops,
     labels = labels)
-ax.set_title('World New Cases',fontsize=20)
-# names for axes
-for ax in [ax]:
-    ax.set_ylabel('case distribution')
 
-# horizontal grid
-ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
-               alpha=0.5)
-
+ax1.set_title('World New Cases',fontsize=20)
+    # names for axes
+ax1.set_ylabel('case distribution')
+    # horizontal grid
+ax1.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
 plt.show()
 
 #You have successfully plotted both new cases and new deaths worldwide.
 world_dates = covid_data.loc[covid_data.loc[:,'location'] == "World", 'date']
 world_new_cases = covid_data.loc[covid_data.loc[:,'location'] == "World", 'new_cases']
-plt.figure(figsize=(7,7))
-plt.plot(world_dates, world_new_cases, 'b+')
+world_new_deaths = covid_data.loc[covid_data.loc[:,'location'] == "World", 'new_deaths']
+fig2 = plt.figure(figsize=(7,7))
+ax2 = fig2.add_axes([0.11,0.20,0.8,0.70])
+plt.plot(world_dates, world_new_cases, 'b.', label = 'world_new_cases')
+plt.plot(world_dates, world_new_deaths, 'r+', label = 'world_new_deaths')
 plt.xticks(world_dates.iloc[0:len(world_dates):4],rotation=-90)
+plt.title("new cases and new deaths worldwide")
+plt.xlabel("date")
+plt.ylabel("number")
+plt.legend()
 plt.show()
 
 #There is code to answer the question stated in file question.txt
@@ -59,13 +76,14 @@ world_dates = covid_data.loc[covid_data.loc[:,'location'] == "World", 'date']
 Spain_new_cases = covid_data.loc[covid_data.loc[:,"location"] == "Spain",'new_cases']
 Spain_total_cases = covid_data.loc[covid_data.loc[:,"location"] == "Spain",'total_cases']
 fig = plt.figure(figsize=(7,7))
-ax1 = fig.add_axes([0.13,0.17,0.8,0.75])
-ax1.plot(world_dates, Spain_new_cases, 'b+', label = 'Spain_new_cases')
-ax1.plot(world_dates, Spain_total_cases, 'r+', label = 'Spain_total_cases')
+ax3 = fig.add_axes([0.13,0.17,0.8,0.75])
+ax3.plot(world_dates, Spain_new_cases, 'c^', label = 'Spain_new_cases')
+ax3.plot(world_dates, Spain_total_cases, 'mv', label = 'Spain_total_cases')
 plt.xticks(world_dates.iloc[0:len(world_dates):4],rotation=-90)
-ax1.set_ylabel('number')
-ax1.set_xlabel('date')
-ax1.set_title('development of new cases and total cases in Spain')
-
-ax1.legend()
+ax3.set_ylabel('number')
+ax3.set_xlabel('date')
+ax3.set_title('development of new cases and total cases in Spain')
+plt.xlabel("date")
+plt.ylabel("number")
+ax3.legend()
 plt.show()
