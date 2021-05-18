383f17179c541979281b038a6801e7d980630d3f
Cai-Bee <shuo.20@intl.zju.edu.cn>
2021-05-18 19:52:29 +0800

    move ft.close() forward to save internal storage

--- practical8/get_unknown_function.py
+++ practical8/get_unknown_function.py
@@ -15,16 +15,18 @@ unknown_seq_data = re.findall(r'[^>]+unknown.function[^>]+', data)
 
 #create a new object to contain selected and changed data
 un = open('unknown_function.fa', 'w')
+fr.close()
 
 #excute every unknown_function gene data
 for i in unknown_seq_data:
-    j = i.replace('\n', '$', 1)           # The three steps in the left deleted
-    j = j.replace('\n', '')               # all \n expect the first one between
-    j = j.replace('$', '\n')              # the discription and the sequence.
-    gene_name = re.search(r'gene:(\S*) ?', i).group(1)       #get gene_name from i
+    j = i.replace('\n', '$', 1)           # The three commends in the left deleted #
+    j = j.replace('\n', '')               # all \n expect the first one between    #
+    j = j.replace('$', '\n')              # the discription and the sequence.      #
+#
+    gene_name = re.search(r'gene:(\S*)?', i).group(1)        #get gene_name from i
     seq = re.search(r'\n(.*$)', j).group(1)                  #get gene sequence
     length = str(len(seq))                                   #measure the sequence's length
-
+#
     #generate the gene name and length as the new discribe line
     simplified_name = ">" + gene_name + "          " + length
     #generate new data contains thenameofthegene,thelengthofthe encoded protein and the protein sequence
@@ -32,4 +34,3 @@ for i in unknown_seq_data:
     un.write(k)                           #write unknown_function genes' data into the new file
 
 un.close()
-fr.close()
